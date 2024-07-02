1f1274bb579bd03fb9d0ea9a3eb2d54f5557efab
Your name <your_email@example.com>
2024-07-02 22:36:39 +0300

    created services section

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,14 +1,130 @@
-import "./Services.css"
+import "./Services.css";
+import { FaRulerCombined } from "react-icons/fa";
 
 function Services() {
   return (
-    <div  className="service-hero">
+    <div className="service-hero">
       <div className="service-hero-text">
-        <p>Our expertise spans across various sectors, ensuring that every project is executed with the highest standards of quality and professionalism.</p>
+        <p>
+          Our expertise spans across various sectors, ensuring that every
+          project is executed with the highest standards of quality and
+          professionalism.
+        </p>
       </div>
+      <div className="services">
+        <h1>Our Services</h1>
+        </div>
+        <div className="service-cards">
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+        <div className="services-items">
+          <div className="icon-container">
+            <FaRulerCombined />
+          </div>
+
+          <h2>Architectural Design</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            architecto temporibus cupiditate tempore maiores repellat quibusdam
+            error minus suscipit dolores.
+          </p>
+          <button type="button">Book Now</button>
+        </div>
+      
+        </div>
+       
       
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
